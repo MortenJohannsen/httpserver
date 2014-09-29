afa0062c6782b1a7c42398b3ca09fa3c6e4c1c26
Morten Johannsen <morten.johannsen89@gmail.com>
2014-09-29 14:46:08 +0200

    Step 1a - Static response statusline, header & blankline er tilføjet til HttpService klassen. Ingen entity body er tilføjet endnu. 0 exceptions

--- httpserver/HttpService.cs
+++ httpserver/HttpService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Sockets;
 using System.Text;
@@ -10,6 +11,9 @@ namespace httpserver
     public class HTTPService
     {
         private TcpClient connectionSocket;
+        private string date = DateTime.Now.ToString();
+        private const string CRLF = "\r\n";
+        private const string htmlType = "text/html";
 
         public HTTPService(TcpClient cntsocket)
         {
@@ -19,6 +23,23 @@ namespace httpserver
         public void doIt()
         {
             Console.WriteLine("Der er oprettet forbindelse...");
+            NetworkStream ns = connectionSocket.GetStream();
+            StreamWriter sw = new StreamWriter(ns);
+            sw.AutoFlush = true;
+
+            string statusline = "HTTP/1.0 200 ok" + CRLF;
+            string header1 = "Last-modified: " + date + CRLF;
+            string header2 = "Content-type: " + htmlType + CRLF;
+            string blankline = CRLF;
+
+            sw.WriteLine(statusline);
+
+
+            Console.WriteLine("--- Message sent {0}, {1}, {2}, {3}:", statusline, header1, header2, blankline);
+
+            sw.Close();
+            connectionSocket.Close();
+
         }
 
 
